a8c99259dc7bff4c3cf71e09e2f3ecf3b63b8dd1
Kian <139572037+lowercase78@users.noreply.github.com>
2025-09-30 12:13:41 +0330

    pro config

--- v2rayN/ServiceLib/Global.cs
+++ v2rayN/ServiceLib/Global.cs
@@ -4,11 +4,11 @@ public class Global
 {
     #region const
 
-    public const string AppName = "v2rayN";
+    public const string AppName = "v2rayN-Pro";
     public const string GithubUrl = "https://github.com";
     public const string GithubApiUrl = "https://api.github.com/repos";
-    public const string GeoUrl = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/{0}.dat";
-    public const string SingboxRulesetUrl = @"https://raw.githubusercontent.com/2dust/sing-box-rules/rule-set-{0}/{1}.srs";
+    public const string GeoUrl = "https://raw.githubusercontent.com/Chocolate4U/Iran-v2ray-rules/release/{0}.dat";
+    public const string SingboxRulesetUrl = @"https://raw.githubusercontent.com/Chocolate4U/Iran-sing-box-rules/rule-set/{1}.srs";
 
     public const string PromotionUrl = @"aHR0cHM6Ly85LjIzNDQ1Ni54eXovYWJjLmh0bWw=";
     public const string ConfigFileName = "guiNConfig.json";
